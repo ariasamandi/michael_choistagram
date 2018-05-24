8371ae9965462d34e3066f1200e53a540d68dc8a
Aria Samandi <ariasamandi@gmail.com>
2018-05-24 12:01:29 -0700

    michael_choistagram

--- apps/my_app/urls.py
+++ apps/my_app/urls.py
@@ -9,11 +9,12 @@ urlpatterns = [
     url(r'^dashboard$', views.dashboard),
     url(r'^add_photo$', views.add_photo),
     url(r'^add_friend/(?P<id>\d+)$', views.add_friend),
-    url(r'^remove/(?P<id>\d+)$', views.remove),
+    url(r'^remove/friend/(?P<id>\d+)$', views.remove_friend),
     url(r'^users/(?P<id>\d+)$', views.users),
     url(r'^purchase$', views.purchase),
     url(r'^prestripe$', views.prestripe),
     url(r'^stripe$', views.stripe),
     url(r'^purchase/process$', views.purchase_process),
+    url(r'^remove/post/(?P<id>\d+)$', views.remove_post),
     url(r'^logout$', views.logout),
 ]
--- apps/my_app/views.py
+++ apps/my_app/views.py
@@ -6,6 +6,8 @@ from models import *
 def index(request):
     return render(request, 'my_app/index.html')
 def register(request):
+    if request.method != "POST":
+        return redirect('/')
     print request.POST
     result = User.objects.register_validator(request.POST)
     if 'errors' in result:
@@ -25,6 +27,8 @@ def login(request):
         request.session['user_id'] = result['user'].id
         return redirect('/dashboard')
 def dashboard(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
     if Post.objects.filter(posted_by__id=request.session['user_id']).count() >= User.objects.get(id=request.session['user_id']).post_tokens:
         context={
             'users_posts': Post.objects.filter(posted_by__id=request.session['user_id']).count(),
@@ -46,9 +50,15 @@ def dashboard(request):
         }
     return render(request, 'my_app/dashboard.html', context)
 def add_photo(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    if request.method != "POST":
+        return redirect('/dashboard')
     Post.objects.create(caption=request.POST['caption'], image=request.FILES['image'], posted_by=User.objects.get(id=request.session['user_id']))
     return redirect('/dashboard')
 def users(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
     context = {
         "user": User.objects.get(id=id),
         "friends": User.objects.filter(friends__id=id),
@@ -56,27 +66,46 @@ def users(request, id):
     }
     return render(request, 'my_app/users.html', context)
 def add_friend(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
     user_1 = User.objects.get(id=request.session['user_id'])
     user_2 = User.objects.get(id=id)
     user_1.friends.add(user_2)
     return redirect('/dashboard')
-def remove(request, id):
+def remove_friend(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
     user_1 = User.objects.get(id=request.session['user_id'])
     user_2 = User.objects.get(id=id)
     user_1.friends.remove(user_2)
     return redirect('/dashboard')
 def purchase(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
     return render(request, 'my_app/purchase.html')
 def prestripe(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
     request.session['val'] = str(request.POST['purchase'])
     print str(request.POST['purchase'])
     return redirect('/stripe')
 def stripe(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
     return render(request, 'my_app/stripe.html')
 def purchase_process(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    if request.method != "POST":
+        return redirect('/dashboard')
     user = User.objects.get(id=request.session['user_id'])
     user.post_tokens += int(request.session['val']) * .05
     user.save()
+    return redirect('/dashboard')
+def remove_post(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    Post.objects.get(id=id).delete()
     return redirect('/dashboard')
 def logout(request):
     request.session.clear()
